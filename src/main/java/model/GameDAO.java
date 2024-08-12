8846ca0af435ff27a01fb2ce97cf10ba7501bd70
Heitor Brazil <heitorbrazil1000@gmail.com>
2024-08-11 21:01:38 -0300

    SQLs Corrigidos

--- src/main/java/model/GameDAO.java
+++ src/main/java/model/GameDAO.java
@@ -12,7 +12,6 @@ import javax.sql.DataSource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-// TODO arrumar os constutores
 public class GameDAO {
 	private DataSource dataSource;
 
@@ -29,7 +28,7 @@ public class GameDAO {
 
 		try {
 			conexao = dataSource.getConnection();
-			String sql = "SELECT * FROM games WHERE name LIKE `%?%`";
+			String sql = "SELECT * FROM games WHERE `name` LIKE '%?%';";
 			statement = conexao.prepareStatement(sql);
 			statement.setString(1, name);
 			result = statement.executeQuery();
@@ -54,7 +53,7 @@ public class GameDAO {
 
 		try {
 			conexao = dataSource.getConnection();
-			String sql = "SELECT * FROM games";
+			String sql = "SELECT * FROM games;";
 			statement = conexao.prepareStatement(sql);
 			result = statement.executeQuery();
 			
@@ -76,21 +75,25 @@ public class GameDAO {
 		int result;
 
         String name = request.getParameter("name");
+		String iconLink = request.getParameter("iconLink");
+		String bannerLink = request.getParameter("bannerLink");
         double price = Double.parseDouble( request.getParameter("price") );
         int discount = Integer.parseInt( request.getParameter("discount") );
         double rating = Double.parseDouble( request.getParameter("rating") );
 
         try {
 			conexao = dataSource.getConnection();
-			String sql = "INSERT INTO games (id, name, gameviews, price, purchases, discount, rating) VALUES (?, ?, ?, ?, ?, ?, ?)";
+			String sql = "INSERT INTO games (`id`, `name`, `iconLink`, `bannerLink`, `views`, `price`, `purchases`, `discount`, `rating`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
 			statement = conexao.prepareStatement(sql);
 			statement.setString(1, UUID.randomUUID().toString());
 			statement.setString(2, name);
-			statement.setInt(3, 0);
-			statement.setDouble(4, price);
+			statement.setString(3, iconLink);
+			statement.setString(4, bannerLink);
 			statement.setInt(5, 0);
-			statement.setInt(6, discount);
-			statement.setDouble(7, rating);
+			statement.setDouble(6, price);
+			statement.setInt(7, 0);
+			statement.setInt(8, discount);
+			statement.setDouble(9, rating);
 			result = statement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -110,7 +113,7 @@ public class GameDAO {
 
 		try {
 			conexao = dataSource.getConnection();
-			String sql = "DELETE FROM games WHERE id LIKE `%?%`";
+			String sql = "DELETE FROM games WHERE `id` LIKE '%?%';";
 			statement = conexao.prepareStatement(sql);
 			statement.setString(1, id);
 			result = statement.executeUpdate();
@@ -129,14 +132,20 @@ public class GameDAO {
 		PreparedStatement statement = null;
 		int result;
 		String name = request.getParameter("name");
+		String iconLink = request.getParameter("iconLink");
+		String bannerLink = request.getParameter("bannerLink");
 		double price = Double.parseDouble(request.getParameter("price"));
+		String id = request.getParameter("id");
 
 		try {
 			conexao = dataSource.getConnection();
-			String sql = "UPDATE games SET name = `?`, price = `?` WHERE id LIKE `%?%`";
+			String sql = "UPDATE games SET `name` = '?', `iconLink` = '?', `bannerLink` = '?', `price` = ?,  WHERE `id` LIKE '%?%';";
 			statement = conexao.prepareStatement(sql);
 			statement.setString(1, name);
-			statement.setDouble(2, price);
+			statement.setString(2, iconLink);
+			statement.setString(3, bannerLink);
+			statement.setDouble(4, price);
+			statement.setString(5, id);
 			result = statement.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
